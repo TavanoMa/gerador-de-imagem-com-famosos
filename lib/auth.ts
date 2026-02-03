d7148e560bb4c4d7d1ed0c4bfe75aac6f3c44ccc
TavanoMa <tavanoma@gmail.com>
2026-02-03 13:56:20 -0300

    bug com criação de usuário resolvido

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,14 +1,13 @@
-import "server-only"
-
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
 import { supabaseServer } from "@/lib/supabase-server"
+import "server-only"
 
 export const {
   handlers: { GET, POST },
   auth,
 } = NextAuth({
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: process.env.AUTH_SECRET,
 
   providers: [
     Google({
@@ -17,50 +16,41 @@ export const {
     }),
   ],
 
-  session: {
-    strategy: "jwt",
-  },
+  session: { strategy: "jwt" },
 
   callbacks: {
     async signIn({ user }) {
-      if (!user.email) return false
+      console.log("SIGNIN USER:", user)
+      if (!user.id || !user.email) {
+        console.log("❌ USER INVÁLIDO")
+        return false
+      } 
 
-      await supabaseServer
+      const res = await supabaseServer
         .from("profiles")
-        .upsert(
-          {
-            email: user.email,
-            name: user.name,
-            image: user.image,
-            credits: 5,
-          },
-          { onConflict: "email" }
-        )
+        .upsert({
+          id: user.id,          // 🔥 ISSO É O MAIS IMPORTANTE
+          email: user.email,
+          name: user.name,
+          credits: 5,
+        })
+
+          console.log("UPSERT RESULT:", res)
 
       return true
     },
 
-    async jwt({ token }) {
-      if (!token.userId && token.email) {
-        const { data } = await supabaseServer
-          .from("profiles")
-          .select("id")
-          .eq("email", token.email)
-          .single()
-
-        if (data) {
-          token.userId = data.id
-        }
+    async jwt({ token, user }) {
+      if (user) {
+        token.userId = user.id
       }
-
       return token
     },
 
     async session({ session, token }) {
-      if (session.user && token.userId) {
+      if (session.user) {
         session.user.id = token.userId as string
       }
-
       return session
     },
   },
