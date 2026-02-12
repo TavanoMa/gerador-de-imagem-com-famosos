ab77427b4b3ba8493a005cfcace84bf78210e591
TavanoMa <tavanoma@gmail.com>
2026-02-12 15:11:22 -0300

    improving sitemap

--- src/app/en/layout.tsx
+++ src/app/en/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "AI Celebrity Photo Generator - Your Photo with Famous People ",
+  title: "AI Celebrity Photo Generator - Your Photo with Famous People",
   description: "Create realistic photos next to your favorite celebrities using artificial intelligence. Generate and share amazing images with famous people.",
 };
 
